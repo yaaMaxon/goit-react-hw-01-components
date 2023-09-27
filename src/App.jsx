7ecd2a392446974c56ff69a15949629b8ed28976
Maksym <maks.sansei@gmail.com>
2023-09-27 13:36:40 +0300

    fixed some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from "components/Profile";
-import { Statistics } from "components/Statistics";
-import { FriendList } from "components/FriendList";
-import { TransactionHistory } from "components/TransactionHistory";
+import { Profile } from "components/Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
+import { FriendList } from "components/FriendList/FriendList";
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
 import data from "./data.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json"
