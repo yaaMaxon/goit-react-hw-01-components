9b525868ab4a0852247566d03ea8af35f488737f
Maksym <maks.sansei@gmail.com>
2023-09-29 10:56:10 +0300

    f

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,13 +3,33 @@ import css from "./FriendList.module.css";
 export const FriendList = ({friends}) => {
     return (
         <ul className={css.friendList}>
-{friends.map(({avatar, name, isOnline, id}) => {
+{/* {friends.map(({avatar, name, isOnline, id}) => {
     return (
         <li key={id} className={css.item}>
   <span className={`${css.status} ${isOnline ? css.isOnline : css.notOnline}`}></span>
   <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={css.name}>{name}</p>
 </li>)
+})} */}
+
+{friends.map(({avatar, name, isOnline, id}) => {
+    return (
+        <FriendListItem 
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+        />
+        )
 })}
 </ul>);
+}
+
+const FriendListItem = ({avatar, name, isOnline}) => {
+    return (
+<li className={css.item}>
+  <span className={`${css.status} ${isOnline ? css.isOnline : css.notOnline}`}></span>
+  <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+  <p className={css.name}>{name}</p>
+</li>);
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,8 @@ import css from "./Statistics.module.css";
 export const Statistics = ({title, stats}) => {
     return (
         <section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
+  {/* <h2 className={css.title}>{title}</h2> */}
+  {title && <h2 className={css.title}>{title}</h2>}
 
   <ul className={css.statList}>
     {stats.map (({id, label, percentage}) => {
